6b4c52fd2e2294cba3e505fb8c2632a424fcaab0
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-21 18:49:41 -0700

    Migrate to AndroidX

--- consumer/src/main/java/com/demo/filereceiver/MainActivity.java
+++ consumer/src/main/java/com/demo/filereceiver/MainActivity.java
@@ -3,7 +3,7 @@ package com.demo.filereceiver;
 import android.content.ClipData;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
